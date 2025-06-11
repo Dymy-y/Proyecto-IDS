7236e6cd961f01af67798a7f9fcb50f867ac9586
Dymy-y <garciabarbarathalia@gmail.com>
2025-06-11 12:52:11 -0600

    Implementación de función modificar-producto

--- MenuFarmacia.c
+++ MenuFarmacia.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include "productos.h"
 
+ void modificarProducto(struct Producto productos[], int numProductos) {
+    int id;
+    printf("Ingrese el ID del producto a modificar: ");
+    scanf("%d", &id);
+
+    for (int i = 0; i < numProductos; i++) {
+        if (productos[i].id == id) {
+            printf("Nuevo nombre: ");
+            scanf(" %[^\n]", productos[i].nombre);
+            printf("Nuevo precio: ");
+            scanf("%f", &productos[i].precio);
+            printf("✏️ Producto modificado exitosamente.\n");
+            return;
+        }
+    }
+
+    printf("❌ Producto no encontrado.\n");
+}
+
 int main() {
     struct Producto productos[MAX_PRODUCTOS];
     int numProductos = 0;
     int opcion;
+
+   
 
     do {
         printf("\n|----------- MENÚ -----------|\n");
@@ -27,8 +48,8 @@ int main() {
                 printf("Función buscar producto no implementada aún.\n");
                 break;
             case 3:
-                // modificarProducto(productos, numProductos);
-                printf("Función modificar producto no implementada aún.\n");
+                modificarProducto(productos, numProductos);
+                //printf("Función modificar producto no implementada aún.\n");
                 break;
             case 4:
                 // eliminarProducto(productos, &numProductos);
